8b9c2e6f344f88460de3e41e3b6c5b45b3126784
Jason Jordan <jtjordan13@gmail.com>
2016-07-18 15:44:20 -0400

    Shows controller and routes added

---
+++ app/controllers/shows_controller.rb
@@ -0,0 +1,26 @@
+class ShowsController < ApplicationController
+
+  def show
+    @show = Show.find params[:id]
+  end
+
+  def index
+    @shows = Show.all
+  end
+
+  def create
+
+  end
+
+  def update
+    @show = Show.find params[:id]
+  end
+
+  def destroy
+    @show = Show.find params[:id]
+    @show.destroy
+    respond_to do |format|
+      format.json { render json: { status: :ok} }
+    end
+  end
+end
