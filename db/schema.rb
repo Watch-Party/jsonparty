5e341f950c174379fbd8cdf432dd64a00abaacf1
Jason Jordan <jtjordan13@gmail.com>
2016-07-20 16:52:24 -0400

    Removed old Paperclip Avatar stuff from users table:

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160720175850) do
+ActiveRecord::Schema.define(version: 20160720204901) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -71,10 +71,6 @@ ActiveRecord::Schema.define(version: 20160720175850) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string   "unconfirmed_email"
-    t.string   "avatar_file_name"
-    t.string   "avatar_content_type"
-    t.integer  "avatar_file_size"
-    t.datetime "avatar_updated_at"
     t.string   "avatar"
     t.string   "provider",               default: "email", null: false
     t.string   "uid",                    default: "",      null: false
