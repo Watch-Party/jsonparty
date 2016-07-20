b87dd54c4964c056d25a50958498e187bfadd36d
Jason Jordan <jtjordan13@gmail.com>
2016-07-20 17:33:39 -0400

    Finished fixing devise parameter sanitizer for account update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,6 +15,6 @@ class ApplicationController < ActionController::Base
 
    def configure_permitted_parameters
        devise_parameter_sanitizer.permit(:sign_up, keys: [:screen_name, :email, :password, :first_name, :last_name])
-       devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:screen_name, :email, :password, :current_password, :avatar_url, :bio, :location, :first_name, :last_name) }
+       devise_parameter_sanitizer.permit(:account_update, keys: [:screen_name, :email, :password, :current_password, :avatar_url, :bio, :location, :first_name, :last_name])
    end
 end
