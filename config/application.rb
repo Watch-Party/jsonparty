f2e6a4ff88aa0d39abc215a698934012d504928d
Jason Jordan <jtjordan13@gmail.com>
2016-07-21 11:16:29 -0400

    Added :patch to allowed methods with cors

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ module Jsonparty
         resource '*',
           :headers => :any,
           :expose  => ['access-token', 'expiry', 'token-type', 'uid', 'client'],
-          :methods => [:get, :post, :options, :delete, :put]
+          :methods => [:get, :post, :patch, :options, :delete, :put]
       end
     end
     # Settings in config/environments/* take precedence over those specified here.
