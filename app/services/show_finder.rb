d88afe3c2b9354993a14e63502a242fb2fd543c1
Jason Jordan <jtjordan13@gmail.com>
2016-07-22 09:17:53 -0400

    Updated show.title formatting

--- app/services/show_finder.rb
+++ app/services/show_finder.rb
@@ -14,7 +14,7 @@ class ShowFinder
         img_url = 'https://s3.amazonaws.com/watch-party/uploads/fallback/image-not-found.png'
       end
       option = Show.new(
-              title: s["show"]["name"],
+              title: s["show"]["name"].downcase,
               cover_img_url: img_url,
               summary: s["show"]["summary"],
               tvrage_id: s["show"]["id"]
