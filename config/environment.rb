32de2b315291f6a58ce0498ac1e65ebc549c9195
Jason Jordan <jtjordan13@gmail.com>
2016-07-22 15:21:55 -0400

    minor formatting

--- config/environment.rb
+++ config/environment.rb
@@ -7,7 +7,7 @@ Rails.application.initialize!
 ActionMailer::Base.smtp_settings = {
    :user_name => ENV['SENDGRID_USERNAME'],
    :password => ENV['SENDGRID_PASSWORD'],
-  :domain => 'json-party.herokuapp.com',
+   :domain => 'json-party.herokuapp.com',
    :address => 'smtp.sendgrid.net',
    :port => 587,
    :authentication => :plain,
