81c16dbc34f3aacb1f87c14dd0ce16aabeccb93c
Jason Jordan <jtjordan13@gmail.com>
2016-07-22 16:24:04 -0400

    Added remote_avatar_url to devise permit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,6 +15,6 @@ class ApplicationController < ActionController::Base
 
    def configure_permitted_parameters
        devise_parameter_sanitizer.permit(:sign_up, keys: [:screen_name, :email, :password, :first_name, :last_name])
-       devise_parameter_sanitizer.permit(:account_update, keys: [:screen_name, :email, :password, :current_password, :avatar, :bio, :location, :first_name, :last_name])
+       devise_parameter_sanitizer.permit(:account_update, keys: [:screen_name, :remote_avatar_url, :email, :password, :current_password, :avatar, :bio, :location, :first_name, :last_name])
    end
 end
