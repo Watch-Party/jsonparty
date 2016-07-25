46487c260267f167e7c1607d780bc1926bea8157
Jason Jordan <jtjordan13@gmail.com>
2016-07-25 12:56:24 -0400

    Just trying something out

--- app/channels/feeds_channel.rb
+++ app/channels/feeds_channel.rb
@@ -1,5 +1,7 @@
 class FeedsChannel < ApplicationCable::Channel
   def subscribed
     stream_from 'posts'
+    ActionCable.server.broadcast 'posts',
+      content: "Welcome to the room!"
   end
 end
