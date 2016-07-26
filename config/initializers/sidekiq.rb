e294ee966df7d2b0925e6af1d3ef0edcb0e18a9e
Jason Jordan <jtjordan13@gmail.com>
2016-07-26 14:51:05 -0400

    Uncommented sidekiq redis config

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
-# Sidekiq.configure_server do |config|
-#   config.redis = { url: 'redis://redistogo:7eb0545f6b8f9d2e59fe908164890fac@catfish.redistogo.com:10336/' }
-# end
-#
-# Sidekiq.configure_client do |config|
-#   config.redis = { url: 'redis://redistogo:7eb0545f6b8f9d2e59fe908164890fac@catfish.redistogo.com:10336/' }
-# end
+Sidekiq.configure_server do |config|
+  config.redis = { url: 'redis://redistogo:7eb0545f6b8f9d2e59fe908164890fac@catfish.redistogo.com:10336/' }
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = { url: 'redis://redistogo:7eb0545f6b8f9d2e59fe908164890fac@catfish.redistogo.com:10336/' }
+end
