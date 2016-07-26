d56a767f22440ce6b9a34dfb8109f08ae0932de5
Jason Jordan <jtjordan13@gmail.com>
2016-07-26 14:44:18 -0400

    Added worker to process posting posts to feed

---
+++ app/workers/post_broadcast_worker.rb
@@ -0,0 +1,12 @@
+class PostBroadcastWorker
+  include Sidekiq::Worker
+
+  def perform(post_id)
+    post = Post.find post_id
+    ActionCable.server.broadcast "#{post.feed_name}",
+      content:    post.content,
+      username:   post.user.screen_name,
+      thumb_url:  post.user.avatar.thumb.url,
+      timestamp:  Time.at(post.time_in_episode).utc.strftime("%M:%S")
+  end
+end
