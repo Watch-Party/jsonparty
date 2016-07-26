78cb8ecb5ff370a0773e3bb6407e7d0ae5225df9
Jason Jordan <jtjordan13@gmail.com>
2016-07-26 14:45:05 -0400

    Added feed name to posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160725191216) do
+ActiveRecord::Schema.define(version: 20160726173224) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 20160725191216) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "feed_id"
+    t.string   "feed_name"
     t.index ["feed_id"], name: "index_posts_on_feed_id", using: :btree
     t.index ["user_id"], name: "index_posts_on_user_id", using: :btree
   end
