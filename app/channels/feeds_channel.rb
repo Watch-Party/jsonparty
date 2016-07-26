ec0804dadd915b8c59382144e0b4b7bcc80167e6
Jason Jordan <jtjordan13@gmail.com>
2016-07-26 11:49:10 -0400

    Working posts and updated feeds for action cable

--- app/channels/feeds_channel.rb
+++ app/channels/feeds_channel.rb
@@ -1,14 +1,36 @@
 class FeedsChannel < ApplicationCable::Channel
   def subscribed
+    show = params["data"].first["show"]
+    season = params["data"].first["season"]
+    episode = params["data"].first["episode"]
     stop_all_streams
-    stream_from 'posts'
+    stream_from "#{show}:s#{season}:e#{episode}"
   end
 
   def unsubscribed
     stop_all_streams
   end
 
-  def send
-    binding.pry
+  def post(data)
+    show = params["data"].first["show"]
+    season = params["data"].first["season"]
+    episode = params["data"].first["episode"]
+    content = data["message"]["content"]
+    user = User.find params["data"].last["user_id"]
+    feed = Feed.first
+
+    post = feed.posts.new(
+                          content: content,
+                          time_in_episode: Time.now - feed.episode.air_date,
+                          user: user
+                          )
+    post.save
+
+
+    ActionCable.server.broadcast "#{show}:s#{season}:e#{episode}",
+      content:    post.content,
+      username:   post.user.screen_name,
+      thumb_url:  post.user.avatar.thumb.url,
+      timestamp:  Time.at(post.time_in_episode).utc.strftime("%M:%S")
   end
 end
