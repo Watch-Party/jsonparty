87071bb9c2462eeedb92090f4c901efc000120be
Jason Jordan <jtjordan13@gmail.com>
2016-07-26 15:14:34 -0400

    Switched to heroku redis

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,9 @@
 require 'sidekiq'
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://redistogo:7eb0545f6b8f9d2e59fe908164890fac@catfish.redistogo.com:10336/', :size => 1 }
+  config.redis = {  }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://redistogo:7eb0545f6b8f9d2e59fe908164890fac@catfish.redistogo.com:10336/', :size => 2 }
+  config.redis = {  }
 end
