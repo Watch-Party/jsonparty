ac52275ca3ba7189c811294ee339600bc47dfc6e
Jason Jordan <jtjordan13@gmail.com>
2016-07-26 15:04:48 -0400

    Further redis/sidekiq config

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,9 @@
+require 'sidekiq'
+
 Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://redistogo:7eb0545f6b8f9d2e59fe908164890fac@catfish.redistogo.com:10336/' }
+  config.redis = { url: 'redis://redistogo:7eb0545f6b8f9d2e59fe908164890fac@catfish.redistogo.com:10336/', :size => 1 }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://redistogo:7eb0545f6b8f9d2e59fe908164890fac@catfish.redistogo.com:10336/' }
+  config.redis = { url: 'redis://redistogo:7eb0545f6b8f9d2e59fe908164890fac@catfish.redistogo.com:10336/', :size => 2 }
 end
