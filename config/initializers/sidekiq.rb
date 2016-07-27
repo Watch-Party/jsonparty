17a26d56b95c09c559b0d1764d3ba7f24733beb3
Jason Jordan <jtjordan13@gmail.com>
2016-07-27 13:24:07 -0400

    Updated sidekiq with more specific redis to go stuff

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,10 +1,10 @@
 require 'sidekiq'
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV["REDIS_PROVIDER"] }
+  config.redis = { url: ENV["REDISTOGO_URL"] }
   config.average_scheduled_poll_interval = 3
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV["REDIS_PROVIDER"] }
+  config.redis = { url: ENV["REDISTOGO_URL"] }
 end
