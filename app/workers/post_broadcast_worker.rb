b74df54778675f437eeb649df25e55214ae618c8
Jason Jordan <jtjordan13@gmail.com>
2016-07-27 17:18:20 -0400

    Added post.id to broadcasts, to help with poping a post

--- app/workers/post_broadcast_worker.rb
+++ app/workers/post_broadcast_worker.rb
@@ -4,6 +4,7 @@ class PostBroadcastWorker
   def perform(post_id)
     post = Post.find post_id
     ActionCable.server.broadcast "#{post.feed_id}",
+      post_id:    post.id,
       content:    post.content,
       username:   post.user.screen_name,
       thumb_url:  post.user.avatar.thumb.url,
