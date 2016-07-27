44c7eedc966080e911d9ef709876f9639a7161c8
Jason Jordan <jtjordan13@gmail.com>
2016-07-27 09:10:22 -0400

    More sidekiq config

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,6 +2,7 @@ require 'sidekiq'
 
 Sidekiq.configure_server do |config|
   config.redis = { url: ENV["REDIS_URL"] }
+  config.average_scheduled_poll_interval = 3
 end
 
 Sidekiq.configure_client do |config|
