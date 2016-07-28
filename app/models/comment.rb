113b962e6d39c71a02f6e6a64a1aeb22ebc05f96
Jason Jordan <jtjordan13@gmail.com>
2016-07-28 12:08:18 -0400

    Updated comment model with after create commit

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,9 @@
 class Comment < ApplicationRecord
+
+  validates_presence_of :content
+
   belongs_to :post
   belongs_to :user
+
+  after_create_commit { CommentBroadcastWorker.perform_async self.id }
 end
