a0f19668cb07a2310403b741a9f16987c19b8f09
Jason Jordan <jtjordan13@gmail.com>
2016-07-28 18:10:09 -0400

    You guessed it, another current user attempt

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -2,17 +2,17 @@ module ApplicationCable
   class Connection < ActionCable::Connection::Base
     identified_by :current_user
 
-    # def connect
-    #   self.current_user = find_verified_user
-    # end
-    #
-    # protected
-    #   def find_verified_user
-    #     if current_user = User.find params["data"][1]["user_id"]
-    #       current_user
-    #     else
-    #       reject_unauthorized_connection
-    #     end
-    #   end
+    def connect(data)
+      self.current_user = find_verified_user(data)
+    end
+
+    protected
+      def find_verified_user(data)
+        if current_user = User.find params["data"][1]["user_id"]
+          current_user
+        else
+          reject_unauthorized_connection
+        end
+      end
   end
 end
