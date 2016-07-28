4fa5d00296d66b4186fed10c1049f3699eac5b94
Jason Jordan <jtjordan13@gmail.com>
2016-07-28 18:00:21 -0400

    More current user stuff

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -2,17 +2,17 @@ module ApplicationCable
   class Connection < ActionCable::Connection::Base
     identified_by :current_user
 
-    def connect
-      self.current_user = find_verified_user
-    end
-
-    protected
-      def find_verified_user
-        if current_user
-          current_user
-        else
-          reject_unauthorized_connection
-        end
-      end
+    # def connect
+    #   self.current_user = find_verified_user
+    # end
+    #
+    # protected
+    #   def find_verified_user
+    #     if current_user
+    #       current_user
+    #     else
+    #       reject_unauthorized_connection
+    #     end
+    #   end
   end
 end
