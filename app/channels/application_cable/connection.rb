3b584498b34cb6085cf572487e808dd2ece82217
Jason Jordan <jtjordan13@gmail.com>
2016-07-28 17:58:13 -0400

    Another current user attempt

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -3,7 +3,16 @@ module ApplicationCable
     identified_by :current_user
 
     def connect
-      self.current_user = current_user
+      self.current_user = find_verified_user
     end
+
+    protected
+      def find_verified_user
+        if current_user
+          current_user
+        else
+          reject_unauthorized_connection
+        end
+      end
   end
 end
