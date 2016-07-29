ffaf484db73bb62c34658e261bdc795d454589b9
Jason Jordan <jtjordan13@gmail.com>
2016-07-29 15:53:26 -0400

    changed comments from and each to a map

--- app/workers/comment_broadcast_worker.rb
+++ app/workers/comment_broadcast_worker.rb
@@ -4,6 +4,14 @@ class CommentBroadcastWorker
   def perform(comment_id)
     comment = Comment.find comment_id
     post = comment.post
+
+    comments = post.comments.where("time_in_episode <= ?", comment.time_in_episode).map {|c|
+      {comment_id:   c.id,
+      content:      c.content,
+      username:     c.user.screen_name,
+      thumb_url:    c.user.avatar.thumb.url,
+      timestamp:    Time.at(c.time_in_episode).utc.strftime("%M:%S")}}
+
     ActionCable.server.broadcast "#{post.feed_id}",
       post_id:    post.id,
       content:    post.content,
@@ -11,14 +19,7 @@ class CommentBroadcastWorker
       thumb_url:  post.user.avatar.thumb.url,
       timestamp:  Time.at(post.time_in_episode).utc.strftime("%M:%S"),
       pops:       post.get_upvotes.size,
-      comments:   
-                  post.comments.where("time_in_episode <= ?", comment.time_in_episode).each do |c|
-                    {comment_id:   c.id,
-                    content:      c.content,
-                    username:     c.user.screen_name,
-                    thumb_url:    c.user.avatar.thumb.url,
-                    timestamp:    Time.at(c.time_in_episode).utc.strftime("%M:%S")}
-                  end
+      comments:   comments
 
   end
 end
