12549981c37dc413d439adb0392e14b1e8a244d6
Jason Jordan <jtjordan13@gmail.com>
2016-07-30 15:59:50 -0400

    removed before action for home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,5 @@
 class HomeController < ApplicationController
   require 'github/markup'
-  skip_before_action :authenticate_user!
 
   def readme
     @html = GitHub::Markup.render('README.md', File.read("README.md"))
