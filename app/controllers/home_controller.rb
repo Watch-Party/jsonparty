f4ec200ea4c9b3b521535ea1efd4212d931e26ae
Jason Jordan <jtjordan13@gmail.com>
2016-07-30 14:21:30 -0400

    More admin devise setup

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   require 'github/markup'
-  skip_before_action :set_format
+  # skip_before_action :set_format
 
   def readme
     @html = GitHub::Markup.render('README.md', File.read("README.md"))
