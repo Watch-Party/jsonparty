131f0074bec73d7006a3d005e23bd0ec31162086
Jason Jordan <jtjordan13@gmail.com>
2016-08-01 17:30:29 -0400

    Removed unused methods in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,16 +2,5 @@ class UsersController < ApplicationController
   include DeviseTokenAuth::Concerns::SetUserByToken
   before_action :set_format
 
-  def show
-    @user = User.find params[:id]
-  end
-
-  def destroy
-    @user = User.find params[:id]
-    @user.destroy
-    respond_to do |format|
-      format.json { render json: { status: :ok} }
-    end
-  end
 
 end
