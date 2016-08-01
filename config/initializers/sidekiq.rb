df036cd047e339b6536cf4b79a434930eb4ef197
Jason Jordan <jtjordan13@gmail.com>
2016-08-01 11:17:11 -0400

    Changed to redis to go for persistence

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,10 +1,10 @@
 require 'sidekiq'
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV["REDIS_URL"] }
+  config.redis = { url: ENV["REDISTOGO_URL"] }
   config.average_scheduled_poll_interval = 3
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV["REDIS_URL"] }
+  config.redis = { url: ENV["REDISTOGO_URL"] }
 end
