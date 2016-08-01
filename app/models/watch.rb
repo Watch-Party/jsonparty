686c115d5705d8de1287c19f1d77c048fc935874
Jason Jordan <jtjordan13@gmail.com>
2016-08-01 16:38:10 -0400

    Can only watch a person once

--- app/models/watch.rb
+++ app/models/watch.rb
@@ -1,4 +1,5 @@
 class Watch < ApplicationRecord
+  validates_uniqueness_of :watcher, scope: :watched
 
   belongs_to :watcher, foreign_key: 'watcher_id', class_name: 'User'
   belongs_to :watched, foreign_key: 'watched_id', class_name: 'User'
