2b7dc809f1550c0b2621a6f1e10476de2ee4ff29
Jason Jordan <jtjordan13@gmail.com>
2016-08-01 17:24:11 -0400

    Trying to get sidekiq to check redis more often

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,7 +2,7 @@ require 'sidekiq'
 
 Sidekiq.configure_server do |config|
   config.redis = { url: ENV["REDISTOGO_URL"] }
-  config.average_scheduled_poll_interval = 3
+  config.average_scheduled_poll_interval = 1
 end
 
 Sidekiq.configure_client do |config|
