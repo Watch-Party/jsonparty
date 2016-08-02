13c39391c1ac8048fffd38102c4be6a515ef615c
Jason Jordan <jtjordan13@gmail.com>
2016-08-02 16:13:42 -0400

    cleaned up and added comments to routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,27 +5,18 @@ Rails.application.routes.draw do
   # Serve websocket cable requests in-process
   mount ActionCable.server => '/cable'
 
-  # resources :feeds
-
-  # devise_for :users, :controllers => {sessions: 'sessions', registrations: 'registrations'}
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  #github style markdown for homepage
   root 'home#readme'
 
+  #sidekiq sinatra page (with work arounds for current incompatibility)
   require 'sidekiq/web'
   Sidekiq::Web.instance_variable_get(:@middleware).delete_if { |klass,_,_| klass == Rack::Protection }
   Sidekiq::Web.set :protection, except: :content_security_policy
   Sidekiq::Web.set :session_secret, Rails.application.secrets[:secret_key_base]
   mount Sidekiq::Web => '/sidekiq'
 
-  resources :users, only: [:show]
-  # resources :watches, only: [:create, :destroy]
 
-  # resources :shows, except: [:new, :edit] do
-  #   resources :episodes, except: [:new, :edit], shallow: true do
-  #     resources :posts, except: [:new, :edit], shallow: true
-  #   end
-  # end
+  #routes for admin site
   get '/admins/admin/:id' => 'admins#show'
   get '/admins/show/new' => 'admins/shows#new'
   get '/admins/show/comfirm' => 'admins/shows#confirm'
@@ -36,6 +27,8 @@ Rails.application.routes.draw do
   patch '/admins/show/:id/activate' => 'admins/shows#activate'
   get '/admins/shows' => 'admins/shows#index'
 
+  #routes for spoileralert api
+  resources :users, only: [:show]
   get '/party/:episode_id' => 'episodes#new_party'
   get  '/:showname/:season/:episode/posts' => 'posts#index'
   get '/:showname/:season/:episode/' => 'episodes#get_id'
@@ -48,6 +41,6 @@ Rails.application.routes.draw do
   get '/episodes/:id' => 'episodes#info'
   get '/upcoming' => 'episodes#upcoming'
   get '/recent' => 'shows#recent_for_user'
-  # get '/search' => 'search#search'
+  # get '/search' => 'search#search'  currently not in use
 
 end
