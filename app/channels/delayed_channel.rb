a0c55030153eca8fdc5d83a8bd0f162d3eefa16c
Jason Jordan <jtjordan13@gmail.com>
2016-08-02 10:12:21 -0400

    first pass of cleaning up delayed channel

--- app/channels/delayed_channel.rb
+++ app/channels/delayed_channel.rb
@@ -1,22 +1,17 @@
 class DelayedChannel < ApplicationCable::Channel
   def subscribed
-    # show = params["data"][0]["show"]
-    # season = params["data"][0]["season"]
-    # episode = params["data"][0]["episode"]
-
-    episode_id = params["data"][0]["episode_id"]
-
-    unless user = User.find(params["data"][1]["user_id"])
-      reject
-    end
-
-    viewtype = params["data"][2]["viewtype"]
 
     stop_all_streams
 
-    # show = Show.find_by(title: show)
+    #initialization
+    unless user = User.find(params["data"][1]["user_id"])
+      reject
+    end
+    episode_id = params["data"][0]["episode_id"]
     episode = Episode.find(episode_id)
+    viewtype = params["data"][2]["viewtype"]
 
+    #create personal feed and stream from it
     feed = episode.feeds.create(
                             species: "delayed",
                             start_time: Time.now,
@@ -25,6 +20,7 @@ class DelayedChannel < ApplicationCable::Channel
 
     stream_from "#{feed.id}"
 
+    #queue up posts for delayed
     df = DelayedFeed.new feed, viewtype, user
     df.start
 
@@ -35,18 +31,14 @@ class DelayedChannel < ApplicationCable::Channel
   end
 
   def post(data)
-    # show = params["data"][0]["show"]
-    # season = params["data"][0]["season"]
-    # episode = params["data"][0]["episode"]
 
+    #initialization
+    user = User.find params["data"][1]["user_id"]
     episode_id = params["data"][0]["episode_id"]
-
+    feed = Feed.where(name: "#{episode_id}:#{user.id}").last
     content = data["message"]["content"]
 
-    user = User.find params["data"][1]["user_id"]
-
-    feed = Feed.where(name: "#{episode_id}:#{user.id}").last
-
+    #create post and send it to broadcast worker
     post = feed.posts.new(
                           content: content,
                           time_in_episode: Time.now - feed.start_time,
@@ -57,30 +49,27 @@ class DelayedChannel < ApplicationCable::Channel
   end
 
   def pop(data)
+
+    #initialization
     user = User.find params["data"][1]["user_id"]
-
     episode_id = params["data"][0]["episode_id"]
-
     feed = Feed.where(name: "#{episode_id}:#{user.id}").last
-
     post = Post.find(data["message"]["post_id"])
-    post.upvote_by user
 
+    #pop(upvote) post and sent to broadcast worker
+    post.upvote_by user
     PopBroadcastWorker.perform_async post.id, user.id, feed.id
 
   end
 
   def comment(data)
     user = User.find params["data"][1]["user_id"]
-
-    post = Post.find(data["message"]["post_id"])
-
     episode_id = params["data"][0]["episode_id"]
-
     feed = Feed.where(name: "#{episode_id}:#{user.id}").last
-
+    post = Post.find(data["message"]["post_id"])
     content = data["message"]["content"]
 
+    #comments made before feed start are time_in_episode = 0
     comment = post.comments.new(
                                 content: content,
                                 user: user,
