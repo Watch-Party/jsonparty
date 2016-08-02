c715ebfa70d4f5ea452a24de869f7b21cbff7149
Jason Jordan <jtjordan13@gmail.com>
2016-08-02 09:24:44 -0400

    Added users#show back in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,8 @@ class UsersController < ApplicationController
   include DeviseTokenAuth::Concerns::SetUserByToken
   before_action :set_format
 
+  def show
+    @user = User.find params[:id]		
+  end
 
 end
