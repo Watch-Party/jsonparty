fec2bc21e799a8ec278a7107a211abbd83332c12
Jason Jordan <jtjordan13@gmail.com>
2016-08-02 11:01:41 -0400

    Removed unneeded methods in feeds controller

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -2,12 +2,4 @@ class FeedsController < ApplicationController
   include DeviseTokenAuth::Concerns::SetUserByToken
   before_action :set_format
 
-  def show
-    @feed = Feed.find(params[:id]).includes(:user)
-  end
-
-  def create
-
-  end
-
 end
