d4a5013f6a64ccf9a7c0c2b1c9fd74fd34ed2f1d
Jason Jordan <jtjordan13@gmail.com>
2016-08-03 12:22:36 -0400

    users autowatch official watch party account

--- app/models/user.rb
+++ app/models/user.rb
@@ -33,9 +33,12 @@ class User < ApplicationRecord
   has_many :watched_watches, foreign_key: :watcher_id, class_name: 'Watch'
 
   #user watches themselves (for filtering on front end purposes) after creation
+  #user autowatches offical watch party user
   after_create_commit {
                         Watch.create(watcher: self,
-                                      watched: self)
+                                      watched: self),
+                        Watch.create(watcher: self,
+                                      watched_id: 6)
                        }
 
   #does not allow screen names to include certain words (will expand)
