390ccbd417242df775d0975b208575fc1db1cfaa
Jason Jordan <jtjordan13@gmail.com>
2017-03-14 16:44:22 -0400

    added provision that would have season finder skip over shows with no tvrage_id

--- app/services/seasons_finder.rb
+++ app/services/seasons_finder.rb
@@ -11,8 +11,10 @@ class SeasonsFinder
     #API call to tvmaze
     resp = HTTParty.get "http://api.tvmaze.com/shows/#{@show.tvrage_id}/seasons"
 
-    #deletes seasons if the premiere is not announced
-    current_seasons = resp.map { |s| s["premiereDate"]}.delete_if { |date| date.nil?}
-    current_seasons.length
+    if resp.code == 200
+      #deletes seasons if the premiere is not announced
+      current_seasons = resp.map { |s| s["premiereDate"]}.delete_if { |date| date.nil?}
+      current_seasons.length
+    end
   end
 end
