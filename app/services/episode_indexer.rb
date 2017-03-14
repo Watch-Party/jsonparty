cfc83188cd81bc82f7d98ea4e496ce0fe459156e
Jason Jordan <jtjordan13@gmail.com>
2017-03-14 15:26:38 -0400

    Adding the splat back to save to see the errors

--- app/services/episode_indexer.rb
+++ app/services/episode_indexer.rb
@@ -55,7 +55,7 @@ class EpisodeIndexer
                         tvrage_e_id: e["id"],
                         end_time: e["airstamp"].to_time + e["runtime"].to_i.minutes
                         )
-      if epi.save
+      if epi.save!
 
       #queues up creation of a live feed for upcoming episodes (to be created an hour and 20 min before the airtime)
       # if epi.air_date > Time.now
